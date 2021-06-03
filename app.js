af14aef8946b4e001b841725e8d3dccf18001553
AniketBanik <aniketbanik05@gmail.com>
2021-06-03 21:21:48 +0600

    connect with database MongoDB,install mongoose,connect mongoosewhich is for npm

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+//app
 const app = express();
-require('dotenv').config()
+
+//db
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true ,
+    useCreateIndex:true
+}).then (() => console.log("DB Connected"));
+
 
 app.get('/',(req,res)=>{
     res.send("hello from node updated");
