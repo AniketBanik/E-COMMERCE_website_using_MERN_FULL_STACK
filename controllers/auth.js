0b6fe979101587d70adf7bfede468ffabda66ebc
AniketBanik <aniketbanik05@gmail.com>
2021-06-07 15:12:57 +0600

    authenticate users and admin during access

--- controllers/auth.js
+++ controllers/auth.js
@@ -67,3 +67,25 @@ exports.requireSignin = expressjwt({
     algorithms: ["HS256"], // added later otherwise it will show error
     userProperty: "auth"
 });
+
+exports.isAuth = (req,res,next) => {
+    let user = req.profile && req.auth && req.profile._id== req.auth._id;
+    if(!user){
+        return res.status(403).json({
+            error: "Access denied"
+        });
+
+    }
+    next();
+};
+
+exports.isAdmin = (req, res, next) => {
+    if(req.profile.role === 0){
+        return res.status(403).json({
+            error: "Admin user!Access denied"
+        });
+    }
+    next();
+
+};
+
